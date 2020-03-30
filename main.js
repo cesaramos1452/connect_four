acedce6d2e36080c3a4eb3782267946f966c7764
Cesar Ramos <cesaramos1452@yahoo.com>
2020-03-30 16:21:47 -0700

    my connect four game has met minimum requirements

--- main.js
+++ main.js
@@ -1,16 +1,17 @@
 let player = 1;
-let numberOfDisks = 0;
+let numberOfCheckers = 0
 
-let createBlackChecker = (cell) => {
+let createSunChecker = (cell) => {
 let div = document.createElement('div');
-div.style.cssText = 'border: 2px solid black; width: 9vw; height: 9vh; background-color: black; border-radius: 50%'
+div.style.cssText = 'width: 6vw; height: 6vh; border: 2px solid orange; background-color: blue ; border-radius: 50%; background: url("./sun.png"); background-position: center; background-size: cover; background-repeat: no-repeat;'
 cell.appendChild(div)
 }
 
-let CreateRedChecker = (cell) => {
+let CreateEarthChecker = (cell) => {
 let divRed = document.createElement('div');
-divRed.style.cssText = 'border: 2px solid black; width: 9vw; height: 9vh; background-color: red; border-radius: 50%'
+divRed.style.cssText = 'width: 6vw; height: 6vh; border: 2px solid turquoise; background-color: blue ; border-radius: 50%; background: url("./earth.png"); background-position: center; background-size: cover; background-repeat: no-repeat;'
 cell.appendChild(divRed)
+
 }
 
 let boardDisplay = [ 
@@ -22,25 +23,147 @@ let boardDisplay = [
     [null, null, null, null, null, null, null]
 ]
 
+let diagonalLeftCheck=()=>{
+    for(let i = 0; i < boardDisplay.length - 3; i++){
+        for(let j = 0; j < boardDisplay.length - 2; j++){
+            if(boardDisplay[i][j] !== null &&
+                boardDisplay[i][j] === 1 &&
+                boardDisplay[i + 1][j + 1] === 1 &&
+                boardDisplay[i + 2][j + 2] === 1 &&
+                boardDisplay[i + 3][j + 3] === 1){
+                  alert('winner 1');
+                  location.reload();
+            }
+            else if(boardDisplay[i][j] !== null &&
+                boardDisplay[i][j] === 2 &&
+                boardDisplay[i + 1][j + 1] === 2 &&
+                boardDisplay[i + 2][j + 2] === 2 &&
+                boardDisplay[i + 3][j + 3] === 2){
+                    alert('winner 2');
+                    location.reload();
+            }
+               
+        }        
+            
+    }
+}
+
+let diagonalRightCheck=()=>{
+    for(let i = boardDisplay.length - 3; i < boardDisplay.length; i++){
+        for(let j = 0; j <= boardDisplay.length; j++){
+            if(boardDisplay[i][j] !== null &&
+               boardDisplay[i][j] === 1 &&
+               boardDisplay[i - 1][j + 1] === 1 &&
+               boardDisplay[i - 2][j + 2] === 1 &&
+               boardDisplay[i - 3][j + 3] === 1){
+                   alert('winner 1')
+                   location.reload()
+            }
+            else if(boardDisplay[i][j] !== null &&
+                boardDisplay[i][j] === 2 &&
+                boardDisplay[i - 1][j + 1] === 2 &&
+                boardDisplay[i - 2][j + 2] === 2 &&
+                boardDisplay[i - 3][j + 3] === 2){
+                    alert('winner 2')
+                    location.reload()
+             }
+        }
+    }
+}
+
+let verticalCheck=()=>{
+    for(let i = 0; i <boardDisplay.length - 2; i++){
+        for(let j = 0; j < boardDisplay.length + 1; j++){
+            if(boardDisplay[i][j] !== null &&
+               boardDisplay[i][j] === 1 &&
+               boardDisplay[i + 1][j] === 1 &&
+               boardDisplay[i + 2][j] === 1 &&
+               boardDisplay[i + 3][j] === 1){
+                   alert('winner 1')
+                   location.reload()
+                
+            }
+           else if(boardDisplay[i][j] !== null &&
+                boardDisplay[i][j] === 2 &&
+                boardDisplay[i + 1][j] === 2 &&
+                boardDisplay[i + 2][j] === 2 &&
+                boardDisplay[i + 3][j] === 2){
+                    alert('winner 2')
+                    location.reload()
+                 
+             }
+        }
+          
+    }    
+}
+
+let horizontalCheck=()=>{
+    for(let i = 0; i < boardDisplay.length; i++){
+        for(let j = 0; j < boardDisplay.length - 2; j++){
+            if(boardDisplay[i][j] !== null &&
+               boardDisplay[i][j] === 1 &&
+               boardDisplay[i][j + 1] === 1 &&
+               boardDisplay[i][j + 2] === 1 &&
+               boardDisplay[i][j + 3] === 1){
+                  alert('winner 1')
+                  location.reload()
+               
+
+            }
+            else if(boardDisplay[i][j] !== null &&
+                boardDisplay[i][j] === 2 &&
+                boardDisplay[i][j + 1] === 2 &&
+                boardDisplay[i][j + 2] === 2 &&
+                boardDisplay[i][j + 3] === 2){
+                   alert('winner 2')
+                   location.reload()
+                
+ 
+             }
+            
+
+            
+        }
+    }
+}
+
+console.log(boardDisplay)
 let updateBoard=()=>{
     let currentColumnId = Number(event.currentTarget.id.slice(-1));
    for(let i = boardDisplay.length-1; i >= 0; i--){
        if(boardDisplay[i][currentColumnId] === null){
            boardDisplay[i][currentColumnId] = player;
-        //    console.log(boardDisplay[i][currentColumnId])
+           console.log(boardDisplay[i][currentColumnId])
            break
        }
    }
     
 }
 
-let insertBlackChecker=()=>{
+
+
+
+let changeColor=()=>{
+    let four = document.querySelectorAll('.change');
+    if(player === 1){
+    four[0].style.color = 'orange';
+    four[1].style.color = 'orange';
+    }
+    else{
+        four[0].style.color = 'turquoise';
+        four[1].style.color = 'turquoise';
+    }
+
+    
+}
+let insertSunChecker=()=>{
     let cells = event.currentTarget;
     if(player === 1){
-        cellArray = cells.children
+        let cellArray = cells.children
         for(let i = 0; i < cellArray.length; i++){
             if(cellArray[i].firstElementChild === null){
-                createBlackChecker(cellArray[i]);
+                createSunChecker(cellArray[i]);
+                cellArray[i].style.backgroundColor = 'orange'
                 break
             }
 
@@ -50,13 +173,14 @@ let insertBlackChecker=()=>{
     }
 }
 
-let insertRedChecker=()=>{
+let insertEarthChecker=()=>{
     let cells = event.currentTarget;
     if(player === 2){
-        cellArray = cells.children
+         let cellArray = cells.children
         for(let i = 0; i < cellArray.length; i++){
             if(cellArray[i].firstElementChild === null){
-                CreateRedChecker(cellArray[i]);
+                CreateEarthChecker(cellArray[i]);
+                cellArray[i].style.backgroundColor = 'turquoise'
                 break
             }
 
@@ -65,28 +189,41 @@ let insertRedChecker=()=>{
         }
     }
 }
-
-
-
-let addDisc=()=>{
-       insertBlackChecker()
-       updateBoard()
-       insertRedChecker()
-       playerSwitch()
-       
-    }
-   
 
 
 let playerSwitch=()=>{
+    
   if(player === 1){
       player = 2
   }
-  else if(player === 2){
+  else{
       player = 1
   }
   
 }
+
+let refreshDom=()=>{
+    let button = document.querySelector('#restart');
+    button.addEventListener('click', function(event){
+        location.reload()
+    })
+
+}
+
+let addDisc=()=>{
+    changeColor()
+    insertSunChecker()
+    updateBoard()
+    insertEarthChecker()
+    playerSwitch()
+    horizontalCheck()
+    verticalCheck()
+    diagonalRightCheck()
+    diagonalLeftCheck()
+    refreshDom();
+    
+ }
+ 
 
 let addClickHandler=()=>{
     let columns = document.querySelectorAll('.column');
@@ -100,48 +237,3 @@ let game=()=>{
     
 }
 game();
-
-
-
-
-
-
-
-
-// if(player){
-        
-//     let columnCell = event.currentTarget.firstElementChild;
-//     if(columnCell.firstElementChild === null){
-//         createBlackChecker(columnCell)
-//         playerSwitch()
-       
-//     }
-//     else if(columnCell.firstElementChild != null){
-//        createBlackChecker(columnCell.nextElementSibling)
-//         playerSwitch()
-        
-//     }
-// }
-// else if(!player){
-//     let columnCell = currentColumn.firstElementChild;
-//     if(columnCell.firstElementChild === null){
-//        CreateRedChecker(columnCell)
-//        playerSwitch()
-       
-//     }
-//     else if(columnCell.firstElementChild != null){
-//        CreateRedChecker(columnCell.nextElementSibling)
-//        playerSwitch()
-//     }
-// }
-
-
-
-
-// let one = document.querySelector('#column1').children
-// console.log(one)
-
-// for(let i = 0; i < one.length; i++){
-//     console.log(one[1])
-//     break;
-// }
